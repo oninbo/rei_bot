fb7dd103cfa2e7f163186047f79e1e11a5e6000e
oninbo <oninbo@gmail.com>
2017-07-06 14:08:37 +0300

    added more phrases

--- bot.py
+++ bot.py
@@ -21,14 +21,23 @@ def set_on(message):
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def say(message):
-    r = random.randint(1, 1000)
-    to_say = False
-    if r <= 200:
-        to_say = True
+def reply(message):
+    print(message.message_id)
     global on
-    if on and to_say:
-        bot.send_message(message.chat.id, content.messages[random.randint(0, len(content.messages)-1)].value)
+    if on and to_say():
+        say(message)
+
+
+def to_say():
+    r = random.randint(1, 1000)
+    if r <= 200:
+        return True
+    else:
+        return False
+
+
+def say(message):
+        print(bot.send_message(message.chat.id, content.messages[random.randint(0, len(content.messages)-1)].value).message_id)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
